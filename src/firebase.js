477d31ffac7d3ba593faa8a27c820d2bf3fed0d9
souravd-vcloudx <sourav.d@vcloudx.com>
2023-08-19 16:49:47 +0530

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,16 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase , set } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_IF,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+console.log("firebase.config::: ", firebaseConfig);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(firebaseApp);;
